90eadf81c085f2d217304bce8448934b9455efb3
22Yash <yashadoke@gmail.com>
2024-10-23 19:33:09 +0530

    login page

---
+++ Login/client/src/New.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const New = () => {
+    return (
+        <div>
+            <h1>Welcome to the Navigation Page</h1>
+            <p>This page is accessible after logging in.</p>
+        </div>
+    );
+};
+
+export default New;
